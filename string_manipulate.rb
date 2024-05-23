29982e55ca9e8f7d8af5ed380fec4e1e1bce4923
greeenboi <suvan.gowrishanker.204@gmail.com>
2024-05-23 19:31:38 +0530

    common string methods

---
+++ string_manipulate.rb
@@ -0,0 +1,15 @@
+# escape characters
+puts "using the apostrophe like \" "
+
+phrase = "   i am not in danger. i am the danger! "
+
+puts phrase.capitalize
+puts phrase.upcase
+puts phrase.length
+puts phrase.include? "danger"
+puts phrase[8,36]
+puts phrase.index("a")
+
+
+
+

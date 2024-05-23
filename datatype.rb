35a7afb5cf31be321218806cb9cdc48ca80cafcf
greeenboi <suvan.gowrishanker.204@gmail.com>
2024-05-23 19:07:48 +0530

    5 main datatypes

---
+++ datatype.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+# string
+
+name = "suvan"
+
+#integer
+
+age = 75
+
+# float / decimal
+
+score = 3.2
+
+# bool
+
+check = true
+iscool = false
+
+# nil / null
+
+dumb = nil
+

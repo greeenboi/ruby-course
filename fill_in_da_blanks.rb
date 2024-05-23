54be4f6609cc0a5c43d385ed346aa0e89df5e030
greeenboi <suvan.gowrishanker.204@gmail.com>
2024-05-23 22:44:48 +0530

    sus version of CAH

---
+++ fill_in_da_blanks.rb
@@ -0,0 +1,6 @@
+puts "Give me an object"
+object = gets.chomp
+puts "now give me a name of a person"
+name = gets.chomp
+
+puts "For my Next Trick, I will pull a " + object + " out of " + name + "."

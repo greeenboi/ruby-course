7eac1ca58964c9f85c0f65299d4f2ca06e0c9dab
greeenboi <suvan.gowrishanker.204@gmail.com>
2024-05-23 21:30:40 +0530

    math methods with strings

--- string_manipulate.rb
+++ string_manipulate.rb
@@ -10,6 +10,13 @@ puts phrase.include? "danger"
 puts phrase[8,36]
 puts phrase.index("a")
 
+my_num = -2234.132
+puts ("This is a number: " + my_num.to_s)
+puts ("This is a number but absolute: " + my_num.abs.to_s)
+puts ("This is a number but absolute n limit decimal: " + my_num.abs.round(2).to_s)
+puts ("This is a number but absolute n rounded up: " + my_num.abs.ceil.to_s)
+puts ("This is a number but absolute n rounded down: " + my_num.abs.floor.to_s)
+
 
 
 

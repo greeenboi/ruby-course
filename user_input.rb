9f169209672f050c8948ea6bd86603a463231ed3
greeenboi <suvan.gowrishanker.204@gmail.com>
2024-05-23 22:29:58 +0530

    test for multiple variables

--- user_input.rb
+++ user_input.rb
@@ -1,4 +1,4 @@
 puts "Enter something breaking bad"
-meth = gets.chomp
+meth, meth2 = gets.chomp.split
 
-puts " this is meth " + meth + "hehehha"
+puts " this is meth " + meth + " hehehha " + meth2

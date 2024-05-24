cbb4c15a21a566498a9be79d77698a0371e69aa9
greeenboi <suvan.gowrishanker.204@gmail.com>
2024-05-24 22:50:44 +0530

    certified hood classic

---
+++ conditional_statements.rb
@@ -0,0 +1,10 @@
+gay = true
+
+this_is_certified_hood_classic = false
+if gay and this_is_certified_hood_classic
+    puts "ya gay boi"
+elsif gay or this_is_certified_hood_classic
+    puts "this is not a certified hood classic"
+else
+    puts "why are you gay"
+end
